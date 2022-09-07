d46db9a3251892be81a04ba636e6516ab9ffac28
Roman Matyushenko <matyushenko.roman.77@outlook.com>
2022-09-07 15:53:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.2</b>";
+  const message="<font color='blue'>Всем привет из STAGING!</font><br><b>App Version 1.2</b>";
   res.status(200).send(message);
 };
